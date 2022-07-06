ebbc0a7dd6ea3bb82d5feb1f51a448dcaf1144a6
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-07-06 14:06:41 +0000

    Add css nd image management

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import 'jquery';
+import './styles/thecore.css';
+import './styles/thecore.scss';
+import CustomerLogo from './images/customer_logo.png';
+import Logo from './images/logo.png';
 
 console.log($)
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,46 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  resolve: {
-    alias: {
-        jquery: "jquery/src/jquery",
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    }
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  mode: 'production'
+    entry: './src/index.js',
+    resolve: {
+        alias: {
+            jquery: "jquery/src/jquery",
+            $: 'jquery/src/jquery',
+            jQuery: 'jquery/src/jquery'
+        }
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            // CSS
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader', 
+                    'css-loader'
+                ],
+            },
+            // SASS
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            // IMAGES
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
