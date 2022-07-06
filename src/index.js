3bf830c53792e06766bc2059a64e2de3336b5aff
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-07-06 14:14:15 +0000

    Add Customer Logo

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ import CustomerLogo from './images/customer_logo.png';
 import Logo from './images/logo.png';
 
 console.log($)
+
+function printLogo(logo) {
+    const myLogo = new Image();
+    myLogo.src = logo;
+    console.log(myLogo);
+}
+
+printLogo(CustomerLogo);
+printLogo(Logo);
