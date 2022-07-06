34f9ded1dc4da97b01d7b7d0003db1ea9184da28
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-07-06 13:31:22 +0000

    Restarting

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,17 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-const config = {
-  target: 'web',
-  entry: {
-    index: './src/thecore.js',
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'thecore.js',
-    library: 'ThecoreUIRailsAdmin',
-    libraryTarget: 'umd',
-    globalObject: 'this',
-    umdNamedDefine: true,
-  },
-  watchOptions: {
-    aggregateTimeout: 600,
-    ignored: /node_modules/,
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanStaleWebpackAssets: false,
-      cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, './dist')],
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: [/node_modules/, /test/],
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-          {
-            loader: 'ts-loader',
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-};
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader', // creates style nodes from JS strings
-          {
-            loader: 'css-loader', // translates CSS into CommonJS
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader', // post process the compiled CSS
-          'sass-loader' // compiles Sass to CSS, using Node Sass by default
-        ]
-      }
-    ]
-  }
+  entry: './src/index.js',
+  resolve: {
+    alias: {
+        jquery: "jquery/src/jquery",
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    }
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production'
 };
